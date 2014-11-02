331e9e40c02db74ebf7ec4f91a93b47612baaeec
taoka <mpr1q7z@gmail.com>
2014-11-02 10:02:11 +0900

    課題完了

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+    a = amount
+    begin
+      bonus = a / 3
+      r = a % 3
+      a = r + bonus
+      total += bonus
+    end until a < 3
+    total
   end
 end
+
+puts BonusDrink.total_count_for(ARGV[0].to_i)
